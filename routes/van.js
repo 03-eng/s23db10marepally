d1bbb77c534d0c92dca74648831a011c3e1463d1
03-eng <teddyreddy789@gmail.com>
2023-04-19 18:01:42 -0500

    updated detail view

--- routes/van.js
+++ routes/van.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var van_controlers= require('../controllers/van');
-/* GET costumes */
+/* GET vans */
 router.get('/', van_controlers.van_view_all_Page );
 router.get('/van/:id', van_controlers.van_detail)
 module.exports = router;
+/* GET detail van page */
+router.get('/detail', van_controlers.van_view_one_Page);
